afeeaf4bfc293de19be9206cd15e7ece2fbc2d04
jpvora29 <47220463+jpvora29@users.noreply.github.com>
2026-06-16 11:07:08 +0530

    Delete config directory

--- config/db_config.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import os
-
-engine = create_engine(f"sqlite:///{os.environ['DB_PATH']}")
-Session = sessionmaker(bind=engine)
